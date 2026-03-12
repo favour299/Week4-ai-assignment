5ed217748d2831b834aaa3bb62f2b448c59967cb
favour299 <black3000gts@gmail.com>
2026-03-12 10:01:21 -0700

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,1 +1,45 @@
+A list is a collection of items stored in order and can be accessed using its position (index).
 
+Example
+
+Python
+
+phones=["Samsung", "iPhone", "Tecno", "Nokia"]
+
+Here:
+
+"Samsung" is index 0
+
+"iPhone" is index 1
+
+"Tecno" is index 2
+
+You access items using the index:
+
+Python
+
+print(phones[0]) # Samsung
+
+A dictionary stores data as key–value pairs. Instead of using positions, you access values using their keys.
+
+Python
+
+phone={
+"brand": "Samsung",
+"model": "A12",
+"price": 15000
+}
+Here:
+
+"brand" → key
+
+"Samsung" → value
+
+You access values using the key:
+
+Python print(phone["brand"]) # Samsung
+
+Simple way to remember
+
+List = items in order
+Dictionary = items with labels (keys).
